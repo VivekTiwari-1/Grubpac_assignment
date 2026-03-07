3b9e1314d684c31467f218ed4d00a66b7a177b40
Vivek Tiwari <vivektiwarii4545@gmail.com>
2026-03-07 07:10:50 +0530

    updated middleware

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -4,20 +4,20 @@ const protectedRoutes = ["/dashboard", "/my-bookings"];
 const authRoutes = ["/login", "/register"];
 
 export function middleware(request) {
-  const { pathname } = request.nextUrl;
-  const token =
-    request.cookies.get("accessToken")?.value ||
-    request.headers.get("x-access-token");
+  // const { pathname } = request.nextUrl;
+  // const token =
+  //   request.cookies.get("accessToken")?.value ||
+  //   request.headers.get("x-access-token");
 
-  const isProtected = protectedRoutes.some((r) => pathname.startsWith(r));
-  const isAuth = authRoutes.some((r) => pathname.startsWith(r));
+  // const isProtected = protectedRoutes.some((r) => pathname.startsWith(r));
+  // const isAuth = authRoutes.some((r) => pathname.startsWith(r));
 
-  if (isProtected && !token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
-  if (isAuth && token) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+  // if (isProtected && !token) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
+  // if (isAuth && token) {
+  //   return NextResponse.redirect(new URL("/dashboard", request.url));
+  // }
 
   return NextResponse.next();
 }
